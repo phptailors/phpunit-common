9fd5bac7e2ea51567d011593bca5bd25a32de640
ptomulik <pawel@tomulik.pl>
2026-05-12 09:24:57 +0000

    cleaned up @param docblocks

--- tests/src/InvalidReturnValueExceptionTest.php
+++ tests/src/InvalidReturnValueExceptionTest.php
@@ -50,12 +50,8 @@ final class InvalidReturnValueExceptionTest extends TestCase
 
     /**
      * @dataProvider provFromExpectedAndActual
-     *
-     * @param array $function
-     *
-     * @psalm-param array{0:object|string,1:string}|callable|string $function
      */
-    public function testFromExpectedAndActual($function, string $expected, string $actual): void
+    public function testFromExpectedAndActual(mixed $function, string $expected, string $actual): void
     {
         $name = self::getFunctionName($function);
         $message = sprintf('Return value of %s() must be %s, %s returned', $name, $expected, $actual);
@@ -91,10 +87,8 @@ final class InvalidReturnValueExceptionTest extends TestCase
 
     /**
      * @dataProvider provFromExpectedTypeAndActualValue
-     *
-     * @param array{0:object|string,1:string}|callable|string $function
      */
-    public function testFromExpectedTypeAndActualValue($function, string $expected, mixed $actual): void
+    public function testFromExpectedTypeAndActualValue(mixed $function, string $expected, mixed $actual): void
     {
         $name = self::getFunctionName($function);
         $actualType = is_object($actual) ? 'object' : gettype($actual);
@@ -104,7 +98,7 @@ final class InvalidReturnValueExceptionTest extends TestCase
         self::assertSame($message, $exception->getMessage());
     }
 
-    protected static function getFunctionName($function): string
+    protected static function getFunctionName(mixed $function): string
     {
         if (is_string($function)) {
             $name = $function;
