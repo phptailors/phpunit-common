b495ee826efdd04fdae8f0454c9f1da312be4972
ptomulik <pawel@tomulik.pl>
2026-05-17 03:50:04 +0000

    make rector & php-cs-fixer happy on 2.1

--- tests/src/Values/RecursiveSelectorVisitorTest.php
+++ tests/src/Values/RecursiveSelectorVisitorTest.php
@@ -708,25 +708,7 @@ final class RecursiveSelectorVisitorTest extends TestCase
         //
         // 07
         //
-        $s07 = new DummyValueSelector(fn ($subject): bool => is_object($subject) && $subject instanceof \Exception, function ($subject, $key, &$retval): bool {
-            if (!is_object($subject) || !$subject instanceof \Exception) {
-                return false;
-            }
-
-            switch ($key) {
-                case 'message':
-                    $retval = $subject->getMessage();
-
-                    return true;
-
-                case 'code':
-                    $retval = $subject->getCode();
-
-                    return true;
-            }
-
-            return false;
-        });
+        $s07 = self::getExceptionPropertySelector();
 
         yield 'RecursiveSelectorVisitorTest.php:'.__LINE__ => [
             'ctor'   => [$selector, new \Exception('foo', 123)],
@@ -831,6 +813,32 @@ final class RecursiveSelectorVisitorTest extends TestCase
                 $retval = $subject[$key];
 
                 return true;
+            }
+        );
+    }
+
+    private static function getExceptionPropertySelector(): DummyValueSelector
+    {
+        return new DummyValueSelector(
+            fn ($subject): bool => is_object($subject) && $subject instanceof \Exception,
+            function ($subject, $key, &$retval): bool {
+                if (!is_object($subject) || !$subject instanceof \Exception) {
+                    return false;
+                }
+
+                switch ($key) {
+                    case 'message':
+                        $retval = $subject->getMessage();
+
+                        return true;
+
+                    case 'code':
+                        $retval = $subject->getCode();
+
+                        return true;
+                }
+
+                return false;
             }
         );
     }
